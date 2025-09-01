04ce03c00214f5ba8bc8573594248ef007157f05
NiuDaLang <gogocfa@yahoo.co.jp>
2025-09-01 18:11:35 +1000

    blogs and search feature

---
+++ assignments/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from .models import About
+
+# Create your views here.
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from blogs.models import Category, Blog
+from assignments.models import About
 
 def home(request):
   # categories = Category.objects.all()
   featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-upcated_at')
   posts = Blog.objects.filter(is_featured=False, status='Published').order_by('-upcated_at')
+
+  # Fetch About Us
+  try: #only work with get()
+    about = About.objects.get()
+  except:
+    about = None
+
   context = {
     # 'categories': categories,
     'featured_posts': featured_posts,
     'posts': posts,
+    'about': about,
   }
   return render(request, "home.html", context)
